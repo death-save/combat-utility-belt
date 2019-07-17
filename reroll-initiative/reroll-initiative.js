f31f9507d82899db66f1d8559018813a514c260a
eclarke12 <therealevan@gmail.com>
2019-07-16 21:53:53 -0700

    reroll-initiative redesign for fvtt 0.3.3

---
+++ reroll-initiative/reroll-initiative.js
@@ -0,0 +1,33 @@
+/**
+ * @author Evan Clarke <errational>
+ * @version 0.0.2
+ * @description Rerolls initiative on combat round change
+ */
+var roundChanged = false;
+
+class RerollInitiative {
+    constructor() {
+        this.postUpdateCombatHook();
+    }
+
+    postUpdateHook() {
+        Hooks.on("updateCombat", (update,combat) =>  {
+            console.log("postupdate:\n",combat);
+            console.log(roundChanged);
+            if(!isBlank(update.round) && update.round > combat.round){
+                this.resetAndReroll(combat);
+            }
+        }); 
+    }
+
+    async resetAndReroll(combat){
+        await combat.resetAll();
+        combat.rollAll();
+    }
+}
+
+let rri = new RerollInitiative();
+
+
+
+
