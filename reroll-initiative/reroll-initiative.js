319a16580354da1224bc372c3d15bbe62429f459
eclarke12 <therealevan@gmail.com>
2019-07-25 20:32:04 -0700

    additional comments/markup

--- reroll-initiative/reroll-initiative.js
+++ reroll-initiative/reroll-initiative.js
@@ -1,10 +1,12 @@
 /**
- * @author Evan Clarke <errational>
+ * @name Reroll-Initiative
  * @version 0.1
+ * @author Evan Clarke <errational>
  * @description Rerolls initiative on combat round change
  */
 
 /**
+ * @name RerollInitiative
  * @author Evan Clarke <errational>
  * @description Hooks on combat update and rerolls initiative for all combatants
  * @todo Add configurability for when to reroll and for whom
@@ -14,15 +16,25 @@ class RerollInitiative {
         this.postUpdateCombatHook();
     }
 
+    /**
+     * @name postUpdateCombatHook
+     * @description Hook on combat update and if round in update is greater than previous -- call resetAndReroll
+     */
     postUpdateCombatHook() {
         Hooks.on("updateCombat", (combat,update) =>  {
+            //Currently the previous round is captured in the 1st index of the Combat._previous array
             if(update.round && update.round > combat._previous[0]){
-                console.log("Reroll-Initiative: Round incremented - Rerolling Initiative")
+                console.log("Reroll-Initiative: Round incremented - rerolling initiative")
                 this.resetAndReroll(combat);
             }
         }); 
     }
 
+    /**
+     * @name resetAndReroll
+     * @param {Combat} combat
+     * @description For the given combat instance, call the resetAll method and the rollAll method
+     */
     async resetAndReroll(combat){
         await combat.resetAll();
         combat.rollAll();
