af74c343afc51f29cdc47b073652fdecece5bda4
eclarke12 <therealevan@gmail.com>
2019-08-13 22:01:10 -0700

    update to use IncarnateApplicationRender - WIP

--- hide-npc-names/hide-npc-names.js
+++ hide-npc-names/hide-npc-names.js
@@ -1,10 +1,25 @@
 Hooks.on("ready", () => {
     hideNPCNames();
+    Application.prototype._render = IncarnateApplicationRender;
 });
+
+const IncarnateApplicationRender = (function () {
+    var cached_function = Application.prototype._render;
+    return function(emptyApp){
+        let resume = Hooks.call("preRender"+this.constructor.name,this);
+        console.log("resume: ",resume);
+        if (resume === false){
+            return new Promise(resolve => null);
+        }else{
+            return cached_function.apply(this, arguments);
+        }
+    }
+})();
+
 
 function hideNPCNames() {
     //hook on combat render
-    Hooks.on("renderCombatTracker", (app,html) => {
+    Hooks.on("preRenderCombatTracker", (app,html) => {
         console.log(app,html);
         // if not GM
         if(!game.user.isGM) {
@@ -27,18 +42,4 @@ function hideNPCNames() {
 
     
 }
-
-const IncarnateApplicationRender = (function () {
-    var cached_function = Application.prototype._render;
-    return function(emptyApp){
-        let resume = Hooks.call("preRender"+this.constructor.name,this);
-        console.log("resume: ",resume);
-        if (resume === false){
-            return new Promise(resolve => null);
-        }else{
-            return cached_function.apply(this, arguments);
-        }
-    }
-})();
-Application.prototype._render = IncarnateApplicationRender;
 
