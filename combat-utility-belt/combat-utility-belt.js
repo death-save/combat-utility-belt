8adedd2d4dbc1e122a6fa77d39b568ed40a87382
eclarke12 <therealevan@gmail.com>
2019-08-16 11:14:16 -0700

    move reroll function into main js

--- combat-utility-belt/combat-utility-belt.js
+++ combat-utility-belt/combat-utility-belt.js
@@ -21,7 +21,83 @@ Hooks.on("ready",  function() {
 }
 
 //reroll initiative
+function cubRerollInitiative() {
+    const FUNCTION_NAME = "reroll-initiative";
+    console.log(FUNCTION_NAME + " starting.");
 
+    let settings = {};
+
+    const DEFAULT_CONFIG = true;
+
+    const SETTINGS_NAME = "Reroll Initiative";
+
+    const SETTINGS_HINT = "Reroll initiative for each combatant every round"
+
+    const SETTINGS_META = {
+        name: SETTINGS_NAME,
+        hint: SETTINGS_HINT,
+        scope: "world",
+        type: Boolean,
+        default: DEFAULT_CONFIG,
+        config: true,
+        onChange: s => {
+            settings = s;
+            console.log(FUNCTION_NAME+" settings changed to", s);
+        }
+    }
+
+    try {
+        settings = getModuleSettings();
+    }
+    catch (e) {
+        if(e.message == "This is not a registered game setting") {
+            registerModuleSettings();
+            settings = getModuleSettings();
+        }
+        else {
+            throw e;
+        }
+
+    }
+
+    function getModuleSettings() {
+        return game.settings.get(FUNCTION_NAME, SETTINGS_NAME);
+    }
+    
+    function registerModuleSettings() {
+        game.settings.register(FUNCTION_NAME, SETTINGS_NAME, SETTINGS_META);
+    }    
+
+    /**
+     * Update module settings and save to the game
+     * @param {String} setting 
+     * @param {*} value 
+     */
+    async function updateSettings(setting, value) {
+        settings[setting] = value;
+        console.log("Updating " + FUNCTION_NAME + " settings:", settings);
+        await game.settings.set(FUNCTION_NAME, SETTINGS_NAME, settings);
+    }
+
+    /**
+     * Hook on update of Combat class. 
+     * Reroll initiative if requirements met
+     */
+    Hooks.on("updateCombat",(async (combat, update) => {
+        const SETTING = "reroll";
+        
+        /**
+         *  firstly is the specified module setting turned on (eg. is rerolling enabled), 
+         *  then test for the presence of the combat object's previous values and an update object,
+         *  check that the round props are numbers,
+         *  finally test if the update's round is greater than the previous combat round 
+         */
+         if(settings && (combat.previous && update) && !isNaN(combat.previous.round || update.round) && update.round > combat.previous.round){
+            await combat.resetAll();
+            combat.rollAll();
+        }
+	}));
+}
 //hide npc names
 
 //enhanced conditions
