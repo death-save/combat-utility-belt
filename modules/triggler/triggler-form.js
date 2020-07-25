1a2ca52ed03e1e64fabb3ed87889af61a0a3500d
eclarke12 <42503461+eclarke12@users.noreply.github.com>
2020-07-24 22:30:32 -0700

    fix actor properties not detected by triggler

--- modules/triggler/triggler-form.js
+++ modules/triggler/triggler-form.js
@@ -47,7 +47,9 @@ export class TrigglerForm extends FormApplication {
         const npcOnly = this.data.npcOnly || null;
         const notZero = this.data.notZero || null;
 
-        const categories = hasProperty(game, "system.template.Actor.templates.common") ? Object.keys(game.system.template.Actor.templates.common) : null;
+        const templates = game.system.template.Actor.templates;
+        const baseTemplate = templates ? Object.values(templates)[0] : null;
+        const categories = baseTemplate ? Object.keys(baseTemplate) : hasProperty(game, "system.template.Actor.templates.common") ? Object.keys(game.system.template.Actor.templates.common) : null;
         const attributes = category ? Object.keys(game.system.template.Actor.templates.common[category]) : null;
         const properties = category && attribute ? Object.keys(game.system.template.Actor.templates.common[category][attribute]) : null;
         const operators = DEFAULT_CONFIG.triggler.operators;
