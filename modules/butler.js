834da5a2545dfd4e0a15bd8a79efaa78c7d92887
Evan Clarke <42503461+eclarke12@users.noreply.github.com>
2020-11-13 22:25:46 -0800

    Closes #345 token effect size

--- modules/butler.js
+++ modules/butler.js
@@ -276,10 +276,10 @@ export const DEFAULT_CONFIG = {
             }
         },
         effectSizeChoices: {
-            xLarge: "Extra Large - 2x2",
-            large: "Large - 3x3",
-            medium: "Medium - 4x4",
-            small: "Small (Default) - 5x5"
+            "small": "Small (Default) - 5x5",
+            "medium": "Medium - 4x4",
+            "large": "Large - 3x3",
+            "xLarge": "Extra Large - 2x2"
         }
     },
     trackerUtility: {
