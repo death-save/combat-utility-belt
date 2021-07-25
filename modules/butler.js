cd4ab324d7d13d3967b75398e50a172e028c64c8
Evan Clarke <42503461+eclarke12@users.noreply.github.com>
2021-07-25 14:42:22 -0700

    Add updateProcessed check to Concentrator updateActor hook

--- modules/butler.js
+++ modules/butler.js
@@ -326,7 +326,8 @@ export const FLAGS = {
         chatMessage: "concentratorChatMessageParsed",
         damageTaken: "damageWasTaken",
         damageAmount: "damageAmount",
-        isDead: "isDead"
+        isDead: "isDead",
+        updateProcessed: "concentrationUpdateProcessed"
     },
     enhancedConditions: {
         conditionId: "conditionId",
